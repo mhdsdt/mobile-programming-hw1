15b4e89d9495c9bdba5e9ccfb0e8cd712e20e9e5
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2025-03-02 22:00:24 +0330

    style: remove redundant import in RetrofitClient

--- src/main/kotlin/api/RetrofitClient.kt
+++ src/main/kotlin/api/RetrofitClient.kt
@@ -1,7 +1,6 @@
 package api
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
